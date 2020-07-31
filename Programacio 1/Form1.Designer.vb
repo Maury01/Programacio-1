4b76655683da5e23c8c8daa761b43a28aa4804a2
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2020-07-31 11:57:30 -0600

    se agrego la funcion de porcentaje

--- Programacio 1/Form1.Designer.vb
+++ Programacio 1/Form1.Designer.vb
@@ -164,7 +164,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(224, 285)
+        Me.ClientSize = New System.Drawing.Size(223, 285)
         Me.Controls.Add(Me.optPorcentaje)
         Me.Controls.Add(Me.optResiduo)
         Me.Controls.Add(Me.optMultiplicacion)
