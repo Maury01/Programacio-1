54d817b099040b0a38864962bcd7a2ef3e93ff15
USIS044220 <usis044220@ugb.edu.sv>
2020-08-19 12:50:02 -0600

    prueba

--- Programacio 1/Form1.Designer.vb
+++ Programacio 1/Form1.Designer.vb
@@ -369,7 +369,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(256, 319)
+        Me.Button1.Location = New System.Drawing.Point(324, 355)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 36
