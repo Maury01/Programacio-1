610091052b10e6d9d27cbbd60e3de4178e8e952c
USIS044220 <usis044220@ugb.edu.sv>
2020-08-19 13:26:20 -0600

    mf

--- Programacio 1/Form1.Designer.vb
+++ Programacio 1/Form1.Designer.vb
@@ -58,6 +58,8 @@ Partial Class Form1
         Me.txtCantTiempo = New System.Windows.Forms.TextBox()
         Me.cboTiempo1 = New System.Windows.Forms.ComboBox()
         Me.lblTiempo = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'lblMoneda
@@ -366,11 +368,31 @@ Partial Class Form1
         Me.lblTiempo.TabIndex = 30
         Me.lblTiempo.Text = "Tiempo"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 36
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(290, 331)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 37
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(682, 416)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnConvertirTiempo)
         Me.Controls.Add(Me.cboTiempo2)
         Me.Controls.Add(Me.txtResultadoTiempo)
@@ -452,4 +474,6 @@ Partial Class Form1
     Friend WithEvents txtCantTiempo As TextBox
     Friend WithEvents cboTiempo1 As ComboBox
     Friend WithEvents lblTiempo As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
